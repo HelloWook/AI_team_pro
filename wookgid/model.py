f82be5c71c3b1961595f57ad8b23ec993eb991c2
남정욱 <113816822+HelloWook@users.noreply.github.com>
2023-05-21 22:31:16 +0900

    모델이랑 연동
    했즘당

--- wookgid/model.py
+++ wookgid/model.py
@@ -7,6 +7,7 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.utils import to_categorical
 
 from tensorflow.keras.models import load_model
+
 """
 
 0 :악성 기형성 콩팥증
@@ -18,31 +19,25 @@ from tensorflow.keras.models import load_model
 6: 혈관종
 
 """
-def predict(pat):
-    model=load_model('C:/Users/swl/Desktop/Code/wookgid/model.h5')
-
-    img_path = 'C:/Users/swl/Desktop/Code/wookgid/bl.jpg'
-
-    # 이미지 로드 및 크기 조정
-    img = load_img(img_path, target_size=(64, 64))
-
-    # 이미지를 배열로 변환
-    img_array = img_to_array(img)
-
-    # 모델 입력 형식에 맞게 배열의 크기 재조정
-    input_array = img_array.reshape((1, 64, 64, 3))
 
 
-    # 모델 예측
-    predict= model.predict(input_array)
-    
-    # 이미지 경로
-    predict_result = np.argmax (predict)
-    return predict_result
+model = load_model("C:/Users/T-user/Desktop/code/123/wookgid/model.h5")
 
+img_path = "C:/Users/T-user/Desktop/code/123/wookgid/templates/bl.jpg"
 
+# 이미지 로드 및 크기 조정
+img = load_img(img_path, target_size=(64, 64))
 
+# 이미지를 배열로 변환
+img_array = img_to_array(img)
 
+# 모델 입력 형식에 맞게 배열의 크기 재조정
+input_array = img_array.reshape((1, 64, 64, 3))
 
+# 모델 예측
+predict = model.predict(input_array)
 
+# 결과
+predict_result = np.argmax(predict)
 
+print(predict_result)
