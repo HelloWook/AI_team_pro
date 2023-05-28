7835aa349152aa23e9ce198eb4eb65f4e0568558
남정욱 <113816822+HelloWook@users.noreply.github.com>
2023-05-28 20:52:51 +0900

    웹 붙이기

--- wookgid/app.py
+++ wookgid/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 import numpy as np
 import os
 import cv2
@@ -17,13 +17,55 @@ def hello():
     return render_template("index.html")
 
 
+@app.route("/0")
+def dis0():
+    img_url = "/static/images/nv.jpg"
+    return render_template("0.html", image_url=img_url)
+
+
+@app.route("/1")
+def dis1():
+    img_url = "/static/images/흑색종jpg.jpg"
+    return render_template("1.html", image_url=img_url)
+
+
+@app.route("/2")
+def dis2():
+    img_url = "/static/images/양성 각화유사 병변.jpg"
+    return render_template("2.html", image_url=img_url)
+
+
+@app.route("/3")
+def dis3():
+    img_url = "/static/images/기저세포암.jpg"
+    return render_template("3.html", image_url=img_url)
+
+
+@app.route("/4")
+def dis4():
+    img_url = "/static/images/신성각화증.jpg"
+    return render_template("4.html", image_url=img_url)
+
+
+@app.route("/5")
+def dis6():
+    img_url = "/static/images/혈관병변.jpg"
+    return render_template("5.html", image_url=img_url)
+
+
+@app.route("/6")
+def dis5():
+    img_url = "/static/images/피부섬유종.jpg"
+    return render_template("6.html", image_url=img_url)
+
+
 @app.route("/", methods=["POST"])
 def predict():
     if request.method == "POST":
         file = request.files["image"]
         image_path = "./images/" + file.filename
         file.save(image_path)
-        model = load_model("C:/Users/swl/Desktop/Code/123/wookgid/model.h5")
+        model = load_model("C:/Users/T-user/Desktop/code/123/wookgid/model.h5")
         img = load_img(image_path, target_size=(64, 64))
         img_array = img_to_array(img)
         input_array = img_array.reshape((1, 64, 64, 3))
@@ -33,4 +75,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
--- wookgid/model.py
+++ wookgid/model.py
@@ -3,27 +3,26 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 from keras.models import load_model
+import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.utils import to_categorical
 
 from tensorflow.keras.models import load_model
 
-"""
 
-0 :악성 기형성 콩팥증
-1: 기저세포암
-2: 양성 섬유종/각질종
-3: 표피성 낭종/낭성내분비증
-4: 흑색종양
-5: 일반적인 피부
-6: 혈관종
-
-"""
-
+lesion_type_dict = {
+    "nv": "Melanocytic nevi",
+    "mel": "Melanoma",
+    "bkl": "Benign keratosis-like lesions ",
+    "bcc": "Basal cell carcinoma",
+    "akiec": "Actinic keratoses",
+    "vasc": "Vascular lesions",
+    "df": "Dermatofibroma",
+}
 
 model = load_model("C:/Users/T-user/Desktop/code/123/wookgid/model.h5")
 
-img_path = "C:/Users/T-user/Desktop/code/123/wookgid/templates/bl.jpg"
+img_path = "C:/Users/T-user/Desktop/code/123/wookgid/templates/diseaseimage/신성각화증.jpg"
 
 # 이미지 로드 및 크기 조정
 img = load_img(img_path, target_size=(64, 64))
@@ -36,8 +35,12 @@ input_array = img_array.reshape((1, 64, 64, 3))
 
 # 모델 예측
 predict = model.predict(input_array)
+probabilities = tf.nn.softmax(predict).numpy()
 
 # 결과
-predict_result = np.argmax(predict)
+predict_result_index = np.argmax(predict)
+predict_class = list(lesion_type_dict.keys())[predict_result_index]
+predict_label = lesion_type_dict[predict_class]
 
-print(predict_result)
+print("Predicted class:", predict_class)
+print("Predicted label:", predict_label)
